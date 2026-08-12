9fb431e3bcb728136a9b671f190b1363c649fe47
Ladinski <borjanladinski18@gmail.com>
2026-08-12 19:55:01 +0200

    Stage 0: setup server and supabase client and Docker

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from app.routers.router import router as task_router
 from app.database.database import initialize_database
+from app.auth.supabase_client import supabase
 
 app = FastAPI(
     title="Task API",
     description="A simple CRUD API for managing tasks.",
     version="1.0.0"
 )
+
+print("Server running and connected to Supabase")
 
 initialize_database()
 
